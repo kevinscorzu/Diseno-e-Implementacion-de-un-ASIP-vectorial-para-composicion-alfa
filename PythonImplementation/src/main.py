bda6e9e8752521fc553ef3e292b6ca0d0c1e23e1
Luis <lulopez@estudiantec.cr>
2021-05-06 16:02:52 -0600

    [ADD] code more like asm

--- PythonImplementation/src/main.py
+++ PythonImplementation/src/main.py
@@ -6,40 +6,49 @@ import cv2
 import matplotlib.pyplot as plt
 
 def generacionDegradado(R,G,B,pos,size):
-    R = R*255
-    G = G*255
-    B = B*255
+    R = R*255/100
+    G = G*255/100
+    B = B*255/100
     #R porcentaje de rojo
     #G porcentaje de verde
     #B porcentaje de azul
-    deg = np.zeros((size,size,3))
+    dR = np.zeros((size,size))
+    dG = np.zeros((size,size))
+    dB = np.zeros((size,size))
     print(deg.shape)
+    Bsize=B/size
+    Gsize=G/size
+    Rsize=R/size
     if(pos=="v"): #horizontal
         for y in range(size):
             for x in range(size):
-                deg[x][y][0] = int(x/size*B)
-                deg[x][y][1] = int(x/size*G)
-                deg[x][y][2] = int(x/size*R)
+                dR[x][y] = int(x)
+                dB[x][y] = int(x)
+                dG[x][y] = int(x)
     if(pos=="h"): #vertical
         for y in range(size):
             for x in range(size):
-                deg[x][y][0] = int(y/size*B)
-                deg[x][y][1] = int(y/size*G)
-                deg[x][y][2] = int(y/size*R)
+                dR[x][y] = int(y)
+                dB[x][y] = int(y)
+                dG[x][y] = int(y)
     if(pos=="d"):#angular
         for y in range(size):
             for x in range(size):
-                deg[x][y][0] = int(x/size*B+y/size*B)
-                deg[x][y][1] = int(x/size*G+y/size*G)
-                deg[x][y][2] = int(x/size*R+y/size*R)
+                dR[x][y] = int(x+y)
+                dB[x][y] = int(x+y)
+                dG[x][y] = int(x+y)
     if(pos=="p"):
         for y in range(size):
             for x in range(size):
-                deg[x][y][0] = int(x/size*B-y/size*B)
-                deg[x][y][1] = int(x/size*G+y/size*G)
-                deg[x][y][2] = int(x/size*R-y/size*R)
-
-    return deg
+                dR[x][y] = int(x-y)
+                dB[x][y] = int(x+y)
+                dG[x][y] = int(x-y)
+    for y in range(size):
+        for x in range(size):
+            dR[x][y] = dR[x][y]*int(Rsize)
+            dB[x][y] = dB[x][y]*int(Rsize)
+            dG[x][y] = dG[x][y]*int(Rsize)
+    return [dR,dG,dB]
 
 def composicionAlfa(transparenciaDegradado, pixelesDegradado):
     imagen = cv2.imread("/home/skryfall/Projects/Diseno-e-Implementacion-de-un-ASIP-vectorial-para-composicion-alfa/PythonImplementation/src/choco.png")
@@ -51,9 +60,12 @@ def composicionAlfa(transparenciaDegradado, pixelesDegradado):
 
     resultado = np.zeros((200,200,3))
 
+    a = (100-transparenciaDegradado)/100
+    b = transparenciaDegradado/100
     for i in range(200):
         for j in range(200):
-            resultado[i][j][0] = int(imagen[i][j][0] * (1 - transparenciaDegradado) + degradado[i][j][0] * transparenciaDegradado)
+            resultado[i][j][0] = int(imagen[i][j][0]*a )
+            resultado[i][j][0] = resultado[i][j][0] + int(degradado[i][j][0] * b)
             resultado[i][j][1] = int(imagen[i][j][1] * (1 - transparenciaDegradado) + degradado[i][j][1] * transparenciaDegradado)
             resultado[i][j][2] = int(imagen[i][j][2] * (1 - transparenciaDegradado) + degradado[i][j][2] * transparenciaDegradado)
 
