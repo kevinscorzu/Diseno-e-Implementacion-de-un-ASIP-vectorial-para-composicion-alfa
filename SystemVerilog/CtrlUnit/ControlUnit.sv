b259c168fdea6cb0e2c2c40115d2f7e51bd2bece
Luis <lulopez@estudiantec.cr>
2021-05-09 15:50:33 -0600

    [ADD] started module conections

--- SystemVerilog/CtrlUnit/ControlUnit.sv
+++ SystemVerilog/CtrlUnit/ControlUnit.sv
@@ -1,15 +1,14 @@
-module ControlUnit(input[1:0] op, input[1:0] inst, input VF,
+module ControlUnit(input[1:0] op, input[1:0] inst, input flagV,
 						output wmemE, rmemE, wregE,CondEn,jmpSel,VF,output[1:0] jmpF,
 						output[2:0] ALUins, output[1:0] ExtndSel);
-	logic[4:0] ALUinstE,ALUinstV;
-	assign ALUinstE = {op,inst};
-	assign wmemE = memF&~VF;
-	assign rmemE = op[0]&op[1]&inst[0]&~inst[1]&~VF;
-	assign wregE = (inst[1]&inst[0] | ~op[1]&op[0] | op[0]&inst[0] + op[0]&inst[1] + op[1]&~op[0])&~VF;
+	assign wmem = op[0]&op[1]&~inst[0]&~inst[1];
+	assign rmemE = op[0]&op[1]&inst[0]&~inst[1];
+	assign wregE = (inst[1]&inst[0] | ~op[1]&op[0] | op[0]&inst[0] + op[0]&inst[1] + op[1]&~op[0]);
 	assign jmpF[0] = ~op[0]&~op[1]&~inst[1];
 	assign jmpF[1] = ~op[0]&~op[1]&inst[0];
 	assign jmpSel = jmpF[1]|jmpF[0];
 	assign CondEn = op[1]&op[0]&inst[1]&inst[0] | ~op[1]&op[0]&~inst[1]&inst[0];
+	assign VF = flagV;
 	ExtendDeco ExtDeco(op, ExtndSel);
-	InstDeco InstDecoO(ALUinstE, ALUinst);
+	InstDeco InstDecoO({op,inst}, ALUins);
 endmodule
---
+++ SystemVerilog/Memories/RegisterFile.sv
@@ -0,0 +1,15 @@
+module RegisterFile (input logic clk,
+							input logic we3,
+							input logic [3:0] ra1, ra2, wa3,
+							input logic [31:0] wd3,
+							output logic [31:0] rd1, rd2);
+					
+	logic [31:0] regs[15:0];
+
+	always_ff @(negedge clk)
+		if (we3) regs[wa3] <= wd3;
+		
+	assign rd1 = regs[ra1];
+	assign rd2 = regs[ra2];
+
+endmodule 
